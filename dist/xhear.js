cc51ae4836cc38730a630e4c7d1175a6f3b57d15
huang yao <kirayao@hotmail.com>
2023-04-06 23:19:12 +0800

    fix: sourcemap file path

--- dist/xhear.js
+++ dist/xhear.js
@@ -3,4 +3,3 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"SZYs":[function(require,module,exports) {
 "use strict";var e=r(require("./render/index.mjs"));function r(e){return e&&e.__esModule?e:{default:e}}function u(r){return(0,e.default)(r)}globalThis.$=u;
 },{"./render/index.mjs":"R4Qp"}]},{},["SZYs"], null)
-//# sourceMappingURL=/xhear.js.map
