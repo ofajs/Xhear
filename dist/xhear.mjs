2d0438690fff8da044ca05732c826bca76804fe0
Yao <kirayao@hotmail.com>
2023-07-23 17:34:17 +0800

    feat: Adding the class method (#60)
    * feat: Adding the class method
    
    Elements can be rendered using class:xxx
    Optimizing on error messages
    The fill item allows to get the element
    
    * chore: bump version

--- dist/xhear.mjs
+++ dist/xhear.mjs
@@ -1,4 +1,4 @@
-//! xhear - v7.2.17 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.2.18 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
@@ -1081,10 +1081,27 @@ const defaultData = {
 
     this.ele.setAttribute(name, value);
   },
+  class(...args) {
+    let [name, value, options] = args;
+
+    if (args.length === 1) {
+      return this.ele.classList.contains(name);
+    }
+
+    value = this._convertExpr(options, value);
+    value = getVal(value);
+
+    if (value) {
+      this.ele.classList.add(name);
+    } else {
+      this.ele.classList.remove(name);
+    }
+  },
 };
 
 defaultData.prop.always = true;
 defaultData.attr.always = true;
+defaultData.class.always = true;
 
 var syncFn = {
   sync(propName, targetName, options) {
@@ -1118,7 +1135,13 @@ var syncFn = {
 var eventFn = {
   on(name, func, options) {
     if (options && options.isExpr && !/[^\d\w_\$\.]/.test(func)) {
+      const oriName = func;
+      
       func = options.data.get(func);
+
+      if (!func) {
+        throw new Error(`${oriName} method does not exist`);
+      }
     } else {
       func = this._convertExpr(options, func);
     }
@@ -2000,13 +2023,14 @@ register({
 });
 
 const createItem = (d, targetTemp, temps, $host) => {
-  const itemData = new Stanz({
-    $data: d,
-    $host,
-  });
-
   const $ele = createXEle(targetTemp.innerHTML);
   const { ele } = $ele;
+
+  const itemData = new Stanz({
+    $data: d,
+    $ele,
+    $host,
+  });
 
   render({
     target: ele,
