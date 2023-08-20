94ba254b3a68ffcb62da8d66d6a4212c1f12793b
huang yao <kirayao@hotmail.com>
2023-08-20 23:31:20 +0800

    test: add mpre recovery case

---
+++ test/cases/memory-recovery/recovery.spec.js
@@ -0,0 +1,57 @@
+// npx playwright codegen http://localhost:3398/test/cases/memory-recovery/demo1.html
+
+import { test, expect } from "@playwright/test";
+
+test("normal recovery", async ({ page }) => {
+  await page.goto(
+    "http://localhost:3398/test/cases/memory-recovery/demo1.html"
+  );
+
+  await page.getByRole("button", { name: "refresh length" }).click();
+  await new Promise((res) => setTimeout(res), 30);
+  expect(await page.$eval("#logger", (node) => node.textContent)).toBe("3");
+
+  await page.getByRole("button", { name: "remove demo" }).click();
+  await page.getByRole("button", { name: "refresh length" }).click();
+  await new Promise((res) => setTimeout(res), 30);
+  expect(await page.$eval("#logger", (node) => node.textContent)).toBe("2");
+
+  await page.getByRole("button", { name: "remove demo" }).click();
+  await page.getByRole("button", { name: "remove demo" }).click();
+  await page.getByRole("button", { name: "refresh length" }).click();
+  await new Promise((res) => setTimeout(res), 30);
+  expect(await page.$eval("#logger", (node) => node.textContent)).toBe("0");
+});
+
+test("transmit data recovery", async ({ page }) => {
+  await page.goto(
+    "http://localhost:3398/test/cases/memory-recovery/demo2.html"
+  );
+
+  await new Promise((res) => setTimeout(res), 30);
+  const { _preview: beforeSize } = await page.waitForFunction(async () => {
+    return outerData.sub.owner.size;
+  });
+  expect(beforeSize).toBe("4");
+
+  await page.getByRole("button", { name: "refresh length" }).click();
+  await new Promise((res) => setTimeout(res), 30);
+  expect(await page.$eval("#logger", (node) => node.textContent)).toBe("6");
+
+  await page.getByRole("button", { name: "remove demo" }).click();
+  await page.getByRole("button", { name: "refresh length" }).click();
+  await new Promise((res) => setTimeout(res), 30);
+  expect(await page.$eval("#logger", (node) => node.textContent)).toBe("4");
+
+  await page.getByRole("button", { name: "remove demo" }).click();
+  await page.getByRole("button", { name: "remove demo" }).click();
+  await page.getByRole("button", { name: "refresh length" }).click();
+  await new Promise((res) => setTimeout(res), 30);
+  expect(await page.$eval("#logger", (node) => node.textContent)).toBe("0");
+
+  await new Promise((res) => setTimeout(res), 30);
+  const { _preview: afterSize } = await page.waitForFunction(async () => {
+    return outerData.sub.owner.size;
+  });
+  expect(afterSize).toBe("1");
+});
