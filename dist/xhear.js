665b414e3e53115fab731ed218403531381842dc
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-08-29 20:24:51 +0800

    chore: bump version

--- dist/xhear.js
+++ dist/xhear.js
@@ -1,4 +1,4 @@
-//! xhear - v7.3.7 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.8 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
--- dist/xhear.mjs
+++ dist/xhear.mjs
@@ -1,4 +1,4 @@
-//! xhear - v7.3.7 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.8 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
