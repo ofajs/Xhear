0537184411338879488aad265503d403c1431121
Yao <kirayao@hotmail.com>
2023-08-31 16:00:40 +0800

    fix: issuse about width and height (#79)
    * fix: issuse about width and height
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- dist/xhear.js
+++ dist/xhear.js
@@ -1,4 +1,4 @@
-//! xhear - v7.3.8 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.9 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -2824,11 +2824,11 @@ try{
     }
 
     get width() {
-      return parseInt(getComputedStyle(this.ele).width);
+      return parseInt(getComputedStyle(this.ele).width) || 0;
     }
 
     get height() {
-      return parseInt(getComputedStyle(this.ele).height);
+      return parseInt(getComputedStyle(this.ele).height) || 0;
     }
 
     get clientWidth() {
