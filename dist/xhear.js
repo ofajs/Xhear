a669c652d5a8b9bc31e2afdfc4c31f028e777c08
Yao <kirayao@hotmail.com>
2023-09-03 15:06:52 +0800

    fix: some issuse (#81)
    * fix: convertToFunc
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- dist/xhear.js
+++ dist/xhear.js
@@ -1,4 +1,4 @@
-//! xhear - v7.3.10 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.11 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -856,6 +856,8 @@ try{
     if(result !== false){
       console.error(error);
     }
+  }else{
+    console.error(error);
   }
 }
 `;
