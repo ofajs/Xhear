2b67b96ab3237d318ac0bd75a9172898cd79a6d0
Yao <kirayao@hotmail.com>
2023-09-28 15:26:24 +0800

    fix: a problem with the fill component (#84)
    * fix: a problem with the fill component
    
    * test: add cases
    
    * chore: bump version
    
    * fix: fill again
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

---
+++ test/cases/230928/230928.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test("fill string item", async ({ page }) => {
+  await page.goto("http://localhost:3398/test/cases/230928/fill-link.html");
+
+  await new Promise((res) => setTimeout(res, 50));
+
+  expect(await page.$eval("li", (node) => node.textContent)).toBe(
+    "en/index.html"
+  );
+
+  await new Promise((res) => setTimeout(res, 600));
+
+  expect(await page.$eval("li", (node) => node.textContent)).toBe(
+    "cn/index.html"
+  );
+});
