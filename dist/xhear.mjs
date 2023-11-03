a3c001d1d8d3e9eefbcc908aa2036653dcc66bf7
Yao <kirayao@hotmail.com>
2023-11-03 09:59:35 -0500

    fix: bug about select element (#89)
    * fix: bug about select element
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- dist/xhear.mjs
+++ dist/xhear.mjs
@@ -1,4 +1,4 @@
-//! xhear - v7.3.17 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.18 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
@@ -1708,6 +1708,10 @@ const initFormEle = ($ele) => {
     case "select":
       {
         const { ele } = $ele;
+        $ele.__unupdate = 1;
+        $ele.value = ele.value;
+        delete $ele.__unupdate;
+
         $ele.watch(() => {
           ele.value = $ele.value;
         });
