96509f14d3b1e8f7d97ce91f92fccfc09ec0c2b5
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-11-03 22:56:54 +0800

    chore: bump version

--- dist/xhear.js
+++ dist/xhear.js
@@ -1,4 +1,4 @@
-//! xhear - v7.3.17 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.18 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -1714,6 +1714,10 @@ try{
       case "select":
         {
           const { ele } = $ele;
+          $ele.__unupdate = 1;
+          $ele.value = ele.value;
+          delete $ele.__unupdate;
+
           $ele.watch(() => {
             ele.value = $ele.value;
           });
