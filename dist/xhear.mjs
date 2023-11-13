4d6ba6e8beaf99d041863739853426102d31a886
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-11-13 16:26:13 +0800

    chore: bump version

--- dist/xhear.mjs
+++ dist/xhear.mjs
@@ -1,4 +1,4 @@
-//! xhear - v7.3.20 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.21 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 const getRandomId = () => Math.random().toString(32).slice(2);
 
 const objectToString = Object.prototype.toString;
