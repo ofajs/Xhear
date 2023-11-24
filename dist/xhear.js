1f2ae4314c717fa4fcf3186ea78e38882cf8803f
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-11-24 23:21:00 +0800

    chore: bump version

--- dist/xhear.js
+++ dist/xhear.js
@@ -1,4 +1,4 @@
-//! xhear - v7.3.21 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.22 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
