26f1100f77ba129cd0e4f6424848c1aedffdc697
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-12-18 20:44:15 +0800

    chore: bump version

--- dist/xhear.js
+++ dist/xhear.js
@@ -1,4 +1,4 @@
-//! xhear - v7.3.24 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
+//! xhear - v7.3.25 https://github.com/kirakiray/Xhear  (c) 2018-2023 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -1295,6 +1295,9 @@ try{
       }
 
       [propName, targetName] = options.beforeArgs;
+
+      propName = hyphenToUpperCase(propName);
+      targetName = hyphenToUpperCase(targetName);
 
       const { data } = options;
 
