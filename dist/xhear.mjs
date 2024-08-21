ee5c1850e424e642417d2414ade19e9bd56d9ebf
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-08-21 23:31:04 +0800

    chore: bump version

--- dist/xhear.mjs
+++ dist/xhear.mjs
@@ -1,4 +1,4 @@
-//! xhear - v7.5.5 https://github.com/ofajs/Xhear  (c) 2018-2024 YAO
+//! xhear - v7.5.6 https://github.com/ofajs/Xhear  (c) 2018-2024 YAO
 // const error_origin = "http://127.0.0.1:5793/errors";
 const error_origin = "https://ofajs.github.io/ofa-errors/errors";
 
