dd93f5e3d6b91989b7d881c1e411edc1a1ffdcd9
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-01-02 21:28:38 +0800

    fix: issuse about fake node

--- dist/xhear.mjs
+++ dist/xhear.mjs
@@ -1,4 +1,4 @@
-//! xhear - v7.5.17 https://github.com/ofajs/Xhear  (c) 2018-2024 YAO
+//! xhear - v7.5.18 https://github.com/ofajs/Xhear  (c) 2018-2025 YAO
 // const error_origin = "http://127.0.0.1:5793/errors";
 const error_origin = "https://ofajs.github.io/ofa-errors/errors";
 
@@ -2783,8 +2783,10 @@ class FakeNode extends Comment {
         } else if (prev === this._start) {
           break;
         }
-      } else {
+      } else if (this.isConnected) {
         throw getErr("xhear_fakenode_unclose", { name: "children" });
+      } else {
+        break;
       }
     }
 
