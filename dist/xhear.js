422df418a6bb2748d155078a88c84eaf38cfe1c0
Yao <kirayao@hotmail.com>
2026-05-25 20:28:58 +0800

    chore: bump version

--- dist/xhear.js
+++ dist/xhear.js
@@ -1,4 +1,4 @@
-//! xhear - v7.5.33 https://github.com/ofajs/Xhear  (c) 2018-2026 YAO
+//! xhear - v7.5.34 https://github.com/ofajs/Xhear  (c) 2018-2026 YAO
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -1184,6 +1184,25 @@
   const getRevokes = (target) => target.__revokes || (target.__revokes = []);
   const addRevoke = (target, revoke) => getRevokes(target).push(revoke);
 
+  const getRenderErrorSupplementary = (data) => {
+    if (!data) {
+      return "";
+    }
+
+    let supplementary = "";
+    if (data.$host || data.$data) {
+      supplementary = "Please check the usage of $host or $data, ";
+    }
+
+    const fromSrc = data.$host?.PATH || data.PATH;
+
+    if (fromSrc) {
+      supplementary += `from file: ${fromSrc}, `;
+    }
+
+    return supplementary;
+  };
+
   const convertToFunc = (expr, data, opts) => {
     const funcStr = `
 const dataRevoked = ${dataRevoked.toString()};
@@ -1253,10 +1272,19 @@ try{
             const expr = dataExpr.replace(/data\((.+)\)/, "$1");
             const func = convertToFunc(expr, data, {
               errCall: (error) => {
-                console.error(
-                  `Error evaluating data() expression in style: "${expr}", style: "${originStyle}", target:`,
-                  target,
+                const supplementary = getRenderErrorSupplementary(data);
+
+                const err = new Error(
+                  `Error evaluating data() expression in style: "${expr}", ${supplementary}`,
+                  {
+                    cause: error,
+                  },
                 );
+
+                console.error(err, {
+                  style: originStyle,
+                  target,
+                });
               },
             });
 
@@ -1301,17 +1329,21 @@ try{
 
       const func = convertToFunc(el.getAttribute("expr"), data, {
         errCall: (error) => {
-          let supplementary = "";
-          if (data.$host || data.$data) {
-            supplementary = "Please check the usage of $host or $data, ";
-          }
+          const supplementary = getRenderErrorSupplementary(data);
 
-          console.error(
-            `Error evaluating text expression: "${el.getAttribute("expr")}", ${supplementary}element:`,
-            textEl,
-            `parent:`,
-            parentNode,
+          const err = new Error(
+            `Error evaluating text expression: '${el.getAttribute("expr")}', ${supplementary}`,
+            {
+              cause: error,
+            },
           );
+
+          console.error(err, {
+            element: textEl,
+            parent: parentNode,
+          });
+
+          return false;
         },
       });
       const renderFunc = () => {
@@ -1357,28 +1389,18 @@ try{
               const func = convertToFunc(expr, data, {
                 errCall: (error) => {
                   const errorExpr = `${actionName === "prop" ? "" : actionName}:${key}="${expr}"`;
+                  const supplementary = getRenderErrorSupplementary(data);
 
-                  let supplementary = "";
-                  if (data.$host || data.$data) {
-                    supplementary = "Please check the usage of $host or $data";
-                  }
-
-                  const err = getErr(
-                    "render_el_error",
+                  const err = new Error(
+                    `Error evaluating element expression: '${errorExpr}', ${supplementary}`,
                     {
-                      expr: errorExpr,
+                      cause: error,
                     },
-                    supplementary
-                      ? new Error(supplementary, { cause: error })
-                      : error,
                   );
 
-                  console.warn(err, {
-                    target: $el.ele,
-                    errorExpr,
+                  console.error(err, {
+                    element: $el.ele,
                   });
-
-                  console.error(err);
 
                   return false;
                 },
